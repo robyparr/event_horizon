f768f2eb655667921d91fbc1616cb7aa1b7d93f6
Roby Parr <roby.parr@gmail.com>
2023-05-16 21:29:31 +0900

    Add 'Unknown' for metrics that cannot be parsed

--- lib/event_horizon_web/controllers/api/event_controller.ex
+++ lib/event_horizon_web/controllers/api/event_controller.ex
@@ -10,16 +10,24 @@ defmodule EventHorizonWeb.Api.EventController do
     site = conn.assigns.current_site
 
     with {:ok, %Event{} = _event} <- Sites.create_event(site, event_params) do
-      user_agent =
-        Plug.Conn.get_req_header(conn, "user-agent")
-        |> List.first()
-        |> UAInspector.parse()
-
-      Sites.increment_metric(site, "device_type", user_agent.device.type)
-      Sites.increment_metric(site, "operating_system", user_agent.os.name)
-      Sites.increment_metric(site, "browser", user_agent.client.name)
-
+      increment_metrics(conn, site)
       send_resp(conn, :created, "")
     end
+  end
+
+  defp increment_metrics(conn, site) do
+    user_agent =
+      Plug.Conn.get_req_header(conn, "user-agent")
+      |> List.first()
+      |> UAInspector.parse()
+
+    device_type = get_in(user_agent, Enum.map(~w[device type]a, &Access.key/1)) || "Unknown"
+    Sites.increment_metric(site, "device_type", device_type)
+
+    operating_system = get_in(user_agent, Enum.map(~w[os name]a, &Access.key/1)) || "Unknown"
+    Sites.increment_metric(site, "operating_system", operating_system)
+
+    browser = get_in(user_agent, Enum.map(~w[client name]a, &Access.key/1)) || "Unknown"
+    Sites.increment_metric(site, "browser", browser)
   end
 end
